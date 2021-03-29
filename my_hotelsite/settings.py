07877fa5450c9d54c3a4ba47d7178bdf84ce8bd9
liammukasa <wmukasa@gmail.com>
2021-03-29 16:59:29 +0300

    Added django-heroku

--- my_hotelsite/settings.py
+++ my_hotelsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django-heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,3 +128,4 @@ MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+django_heroku.settings(locals())
